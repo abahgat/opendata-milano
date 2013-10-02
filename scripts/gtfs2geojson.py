9f9e6aba7bb597a2541263ccbea83653c425ea4a
Alessandro Bahgat <ale.bahgat@gmail.com>
2013-10-01 22:51:41 -0400

    Extract stops from a GTFS feed and convert them to a GeoJSON FeatureCollecion of Points

---
+++ scripts/gtfs2geojson.py
@@ -0,0 +1,21 @@
+import csv
+import geojson
+
+def convert():
+  with open('stops.txt', 'r') as stops_file:
+    reader = csv.DictReader(stops_file)
+    features = {}
+    for row in reader:
+      stop_id = row['stop_id']
+      name = row['stop_name']
+      latitude = row['stop_lat']
+      longitude = row['stop_lon']
+      feature = geojson.Feature(id=stop_id,
+                  							geometry=geojson.Point([latitude, longitude]),
+                  							properties={})
+      features[stop_id] = feature
+  collection = geojson.FeatureCollection(features=features.values())
+  print geojson.dumps(collection)
+
+if __name__ == "__main__":
+  convert()
